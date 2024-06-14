3771bb229832a400a3a9f40560b3f70ff6d0b598
rachmankamil <rachmankamil24@gmail.com>
2024-06-14 20:06:43 +0700

    add page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, BrowserRouter, Link } from "react-router-dom"
 import HomePage from "./pages/home"
 import GroceriesPage from "./pages/groceries"
+import ContactUsPage from './pages/contact-us';
 
 function App() {
   return (
@@ -12,11 +13,13 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/groceries">Groceries</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/groceries" element={<GroceriesPage />} />
+          <Route path='/contact' element={<ContactUsPage />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/contact-us.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+function ContactUsPage() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>Please fill this form to concern your problem, we will answer soon.</p>
+
+            <form action="">
+                <label htmlFor="nameInput">Name : </label>
+                <input type="text" name="name" id="nameInput" />
+                <br />
+                <label htmlFor="emailInput">Email : </label>
+                <input type="email" name="email" id="emailInput" />
+                <br />
+                <label htmlFor="messageArea">Message : </label>
+                <textarea name="message" id="messageArea"></textarea>
+                <br />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    )
+}
+
+export default ContactUsPage
